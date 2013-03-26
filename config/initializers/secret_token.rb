478fec5ee5f44378f63f367b6f3e2d47f70d752d
randomp <randomzpq@gmail.com>
2013-03-25 23:24:12 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FDU08ME::Application.config.secret_token = 'c6054a3f57c54a4171da3ecd81944e84a2f52078d3ba63aa9f3f075e09133e78409dbbf325fff10d24c9beccc3058807c98cecba443318ba70a413573e365359'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FDU08ME::Application.config.session_store :cookie_store, key: '_FDU08ME_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FDU08ME::Application.config.session_store :active_record_store
